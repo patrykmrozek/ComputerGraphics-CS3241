2e5ac4fd084d6ae5cbb3dfee291f869989c1a2ef
patrykmrozek <45935681+patrykmrozek@users.noreply.github.com>
2025-09-02 12:14:51 +0800

    drawSphere()

--- Tutorials/tut1/main.cpp
+++ Tutorials/tut1/main.cpp
@@ -1,19 +1,64 @@
+#include <math.h>
 #include <GLUT/glut.h>
+
+void drawSphere(double r) {
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    //x = (r*sin(Phi)*cos(Theta))
+    //y = (r*sin(Phi)*sin(Theta))
+    //z = (r*cos(Phi))
+    glScalef(r, r, r);
+    int n = 20;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < 2*n; j++) {
+            //AntiClockwise
+            //i, j, i, j TL
+            float phi1 = i*M_PI/n;
+            //i, j+1, i, j+1 BL
+            float theta1 = j*M_PI/n;
+            //i+1, j+1, i+1, j+1 BR
+            float theta2 = (j+1)*M_PI/n;
+            //i+1, j, i+1, j TR
+            float phi2 = (i+1)*M_PI/n;
+            glBegin(GL_QUADS);
+
+            glVertex3f(r*sin(phi1)*cos(theta1),
+                       r*sin(phi1)*sin(theta1),
+                       r*cos(phi1));
+
+            glVertex3f(r*sin(phi1)*cos(theta2),
+                       r*sin(phi1)*sin(theta2),
+                       r*cos(phi1));
+
+            glVertex3f(r*sin(phi2)*cos(theta2),
+                       r*sin(phi2)*sin(theta2),
+                       r*cos(phi2));
+
+            glVertex3f(r*sin(phi2)*cos(theta1),
+                       r*sin(phi2)*sin(theta1),
+                       r*cos(phi2));
+
+            glEnd();
+        }
+    }
+}
+
 
 void display() {
     glClear(GL_COLOR_BUFFER_BIT);
-    glBegin(GL_POLYGON);
-    glVertex2f(-0.5, -0.5);
-    glVertex2f(-0.5, 0.5);
-    glVertex2f(0.5, 0.5);
-    glVertex2f(0.5, -0.5);
-    glEnd();
-    glFlush();
+    //draw_square();
+
+    glColor3f(1.0, 0.0, 0.0);
+    drawSphere(1.0);
+
+    glutSwapBuffers();
 }
 
 
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
+    glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize (800, 800);
+    glutInitWindowPosition (50, 50);
     glutCreateWindow("My  Window");
 
     glutDisplayFunc(display);
@@ -76,10 +121,17 @@ int main(int argc, char** argv) {
  * a) {(13, 16), (1, 10), (25, 10), (15, 6), (11, 2), (15, 2), (22, 1), (4, 1)}
  * b) (13, 15.5)
  * c) LHS: (12, 15.5), RHS: (14 15.5)
- * d) Color from x=12 to x=14
+ * d) Color from x=12 to x=13
  * e) LHS, m = 0.5 so deltaX = -2 : RHS, , = -0.5 so deltaX = 2
  * f) LHS: {(11, 14.5), (9, 13.5), (7, 12.5), (5, 11.5), (3, 10.5), (1, 9.5)}
  *    RHS: {(15, 14.5), (17, 13.5), (19, 12.5), (21, 11.5), (23, 10.5), (24, 9.5)}
  * g) fill from x=approx(3) to x=13, then from x=15 to x=22
+ * 	  keep a parity bit, that will keekp track of whether it is inside or outside the polygon
+ *    each time ther is an intersection, flip the bit - initially false
+ *
+ * h) Every point P on the sphere can be specified by two angles Theta and Phi, radius r
+ * 		x = (r*sin(Phi)*cos(Theta))
+ * 		y = (r*sin(Phi)*sin(Theta))
+ * 		z = (r*cos(Phi))
  *
  */
