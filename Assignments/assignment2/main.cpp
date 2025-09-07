22dd0ceb5cfd23dda0a2d211be432661b2edefad
patrykmrozek <45935681+patrykmrozek@users.noreply.github.com>
2025-09-07 20:04:23 +0800

    removed default code - starting with simple sphere

--- Assignments/assignment2/main.cpp
+++ Assignments/assignment2/main.cpp
@@ -8,45 +8,52 @@
 #include <OpenGL/gl.h>
 #include <GLUT/GLUT.h>
 
-using namespace std;
-
-#define numStars 100
-#define numPlanets 9
-
-
-class planet
-{
-public:
-	float distFromRef;
-	float angularSpeed;
-	GLfloat color[3];
-	float size;
-	float angle;
-	float alpha;
-
-	planet()
-	{
-		distFromRef = 0;
-		angularSpeed = 0;
-		color[0] = color[1] = color[2] = 0;
-		size = 0;
-		angle = 0;
-		alpha = 1.0;
-	}
-};
-
-GLfloat PI = 3.14;
+GLfloat GPI = (GLfloat)M_PI;
 float alpha = 0.0, k=1;
-float tx = 0.0, ty=0.0;
-planet planetList[numPlanets];
+float tx = 0.0, ty=1;
 
 
-bool clockMode = false;
+void drawSphere(double r) {
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    //x = (r*sin(Phi)*cos(Theta))
+    //y = (r*sin(Phi)*sin(Theta))
+    //z = (r*cos(Phi))
+    int n = 20;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < 2*n; j++) {
+            //AntiClockwise
+            //i, j, i, j TL
+            float phi1 = i*M_PI/n;
+            //i, j+1, i, j+1 BL
+            float theta1 = j*M_PI/n;
+            //i+1, j+1, i+1, j+1 BR
+            float theta2 = (j+1)*M_PI/n;
+            //i+1, j, i+1, j TR
+            float phi2 = (i+1)*M_PI/n;
+            glBegin(GL_QUADS);
+
+            glVertex3f(r*sin(phi1)*cos(theta1),
+                       r*sin(phi1)*sin(theta1),
+                       r*cos(phi1));
+
+            glVertex3f(r*sin(phi1)*cos(theta2),
+                       r*sin(phi1)*sin(theta2),
+                       r*cos(phi1));
+
+            glVertex3f(r*sin(phi2)*cos(theta2),
+                       r*sin(phi2)*sin(theta2),
+                       r*cos(phi2));
+
+            glVertex3f(r*sin(phi2)*cos(theta1),
+                       r*sin(phi2)*sin(theta1),
+                       r*cos(phi2));
+
+            glEnd();
+        }
+    }
+}
 
 
-time_t seconds = 0;
-struct tm * timeinfo;
-float timer = 0.1; // this is a dumb way to control the speed of rotations
 
 
 void reshape (int w, int h)
@@ -63,39 +70,18 @@ void reshape (int w, int h)
 
 void init(void)
 {
-	glClearColor (0.0, 0.0, 0.3, 1.0);
+    glClearColor (0.0, 0.0, 0.1, 1.0);
 	glShadeModel (GL_SMOOTH);
 	glEnable(GL_BLEND);
 	glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 }
 
 
-
-void generatePlanets()
-{
-	//The sun
-	planetList[0].distFromRef = 0;
-	planetList[0].angularSpeed = 0;
-	planetList[0].color[0] = 1.0;
-	planetList[0].color[1] = 0.7;
-	planetList[0].color[2] = 0.0;
-	planetList[0].size = 1.5;
-
-	//Mercury
-	planetList[1].distFromRef = 3;
-	planetList[1].angularSpeed = 5;
-	planetList[1].color[0] = 0.7;
-	planetList[1].color[1] = 0.7;
-	planetList[1].color[2] = 0.7;
-	planetList[1].size = 0.5;
-
-
-}
-
-
 void display(void)
 {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    drawSphere(10);
 	
 	glPushMatrix();
 
@@ -103,31 +89,6 @@ void display(void)
 	glScalef(k, k, k);	
 	glTranslatef(tx, ty, 0);	
 	glRotatef(alpha, 0, 0, 1);
-
-	int circle_points = 50;
-	float angle = 0;
-
-
-
-	// You can modify the following for your assignment
-	//draw mercury
-	glPushMatrix();
-	glRotatef(planetList[1].angle, 0, 0, 1);
-	glTranslatef(0, planetList[1].distFromRef, 0);	
-	glBegin(GL_POLYGON);
-
-		for (int i = 0; i < circle_points; i++) 
-		{
-			angle = 2*PI*i/circle_points;
-			if(i>circle_points/2)
-				glColor4f(planetList[1].color[0], planetList[1].color[1], planetList[1].color[2], planetList[1].alpha);
-			else
-				glColor4f(planetList[1].color[0]-0.2, planetList[1].color[1]-0.2, planetList[1].color[2]-0.2, planetList[1].alpha);
-			glVertex2f(planetList[1].size * cos(angle), planetList[1].size *sin(angle));
-		}
-
-	glEnd();
-	glPopMatrix();
 
 
 	glPopMatrix();
@@ -139,35 +100,6 @@ void idle()
 	// this idle function is to adjust the parameters for display() to draw
 	// You can modify the following for your assignment
 
-
-
-	if(!clockMode)
-	{
-		
-		//animate planets
-		for(int i=0;i<numPlanets;i++)
-		{
-			planetList[i].alpha = 1;
-			planetList[i].angle += planetList[i].angularSpeed*timer;
-		}
-
-	}
-	else
-	{
-		// draw your clock here
-
-		seconds = time(NULL);
-		timeinfo = localtime(&seconds);
-
-		for (int i = 0; i < numPlanets; i++)
-		{
-			planetList[i].alpha = 1;
-			planetList[i].angle = ((float)timeinfo->tm_sec  )*6;
-		}
-	}
-	
-	glutPostRedisplay();
-	std::this_thread::sleep_for(std::chrono::milliseconds(100));
 }
 
 void keyboard (unsigned char key, int x, int y)
@@ -181,7 +113,7 @@ void keyboard (unsigned char key, int x, int y)
 		case 'q':
 		case 'Q':
             exit(0);
-	
+    /*
 		case 't':
 			clockMode = !clockMode;
 			if (clockMode)
@@ -189,6 +121,7 @@ void keyboard (unsigned char key, int x, int y)
 			else
 				cout << "Current Mode: Solar mode." << endl;
 			break;
+*/
 
 		default:
 			break;
@@ -197,14 +130,11 @@ void keyboard (unsigned char key, int x, int y)
 
 int main(int argc, char **argv)
 {
-	cout<<"CS3241 Lab 2\n\n";
-	cout<<"+++++CONTROL BUTTONS+++++++\n\n";
-	cout<<"Toggle Time Mode: T\n";
-    cout<<"Exit: ESC or q/Q\n";
-	cout << "Current Mode: Clock mode." << endl;
-
-
-	generatePlanets();
+    std::cout<<"CS3241 Lab 2\n\n";
+    std::cout<<"+++++CONTROL BUTTONS+++++++\n\n";
+    std::cout<<"Toggle Time Mode: T\n";
+    std::cout<<"Exit: ESC or q/Q\n";
+    std::cout << "Current Mode: Clock mode." << "\n";
 
 	glutInit(&argc, argv);
 	glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
